cd6039b636b20c65a03e3820417d105ccfae579e
Akash Singh <akashsingh6280@gmail.com>
2021-10-12 00:05:04 +0530

    ADDED: regex validation and event listener for email

--- Js/SimpleForm.js
+++ Js/SimpleForm.js
@@ -1,8 +1,8 @@
-const NAME_REGEX = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
+const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
 const text = document.querySelector('#text');
 const textError = document.querySelector('.text-error');
 text.addEventListener('input', function() {
-    if (NAME_REGEX.test(text.value)) {
+    if (nameRegex.test(text.value)) {
         textError.textContent = "";
     } else {
         textError.textContent = "Invalid Name";
@@ -14,4 +14,15 @@ const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
 salary.addEventListener('input', function() {
     output.textContent = salary.value;
-})
+});
+
+const emailRegex= RegExp("^[a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9](([.+-_][a-zA-Z0-9]+)?)[@][a-zA-Z0-9]+[.]([a-z]{2,3})([.][a-zA-z]{2})?$");
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input', function() {
+    if (emailRegex.test(email.value)) {
+        emailError.textContent = "";
+    } else {
+        emailError.textContent = "Invalid Email";
+    }
+});
